44b29b8b9dc5df4602ef833047e825419b39483a
Renato <renato.bastos96@gmail.com>
2021-11-13 20:56:08 -0300

    Atualizando Local Storage

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+
+var tarefasLocal = [];
+var indexLocal = 0;
+
 function adicionar() {
   var tarefa = document.getElementById("inputTarefa").value;
   var iconeTrash = '<i class="fas fa-trash"></i>';
 
   if (tarefa != "") {
     var listItem =
-      "<li><input type='checkbox'><span>" +
+      "<li><input type='checkbox'><span class='texto'>" +
       tarefa +
-      "</span> <button class='trash' onclick='remover()'>" +
+      "</span> <button class='trash'>" +
       iconeTrash +
       "</button></li>";
 
@@ -21,7 +25,15 @@ function adicionar() {
       linhas[i].addEventListener("click", removerItem);
     }
 
+    armazenamentoLocal();
+    
+
+    
     document.getElementById("inputTarefa").value = null;
+  
+  
+  
+  
   } else {
     window.alert("Insira uma tarefa!");
   }
@@ -32,4 +44,23 @@ function removerItem(e) {
   e.preventDefault();
   var li = this.parentNode;
   li.parentNode.removeChild(li);
+
+  armazenamentoLocal();
+}
+
+//Atualiza Local Storage
+function armazenamentoLocal(){
+  var span = document.getElementsByClassName("texto");
+    var arraySpan = [];
+
+    for (var i = 0; i < span.length; i++) {
+      arraySpan[i] = span[i].textContent;
+      console.log(arraySpan[i]);
+    }
+
+    var spanStr = JSON.stringify(arraySpan);
+
+    localStorage.setItem("tarefa", spanStr);
+
+    console.log(localStorage.length);
 }
