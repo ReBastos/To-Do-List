c887bbeb7d72ab432716f67a47315f1ef3a56e88
Renato <renato.bastos96@gmail.com>
2021-11-13 16:07:18 -0300

    Itens Apagando Corretamente

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 function adicionar() {
-    var tarefa = document.getElementById("inputTarefa").value;
-    var iconeTrash = '<i class="fas fa-trash"></i>';
+  var tarefa = document.getElementById("inputTarefa").value;
+  var iconeTrash = '<i class="fas fa-trash"></i>';
 
   if (tarefa != "") {
-    
     var listItem =
-      "<li><input type='checkbox'><span>" + tarefa + "</span> <button class='trash' onclick='remover()'>"+iconeTrash+"</button></li>";
+      "<li><input type='checkbox'><span>" +
+      tarefa +
+      "</span> <button class='trash' onclick='remover()'>" +
+      iconeTrash +
+      "</button></li>";
 
     var lista = document.getElementById("uList");
 
     lista.insertAdjacentHTML("beforeend", listItem);
 
+    var linhas = document.getElementsByClassName("trash");
+
+    //Adiciona Evento aos Botões 
+    for (var i = 0; i < linhas.length; i++) {
+      linhas[i].addEventListener("click", removerItem);
+    }
+
     document.getElementById("inputTarefa").value = null;
   } else {
-      window.alert("Insira uma tarefa!");
+    window.alert("Insira uma tarefa!");
   }
 }
 
-function remover(){
-    
-    event.target.parentNode.remove();
+//Remove Elemento da Lista de Tarefas
+function removerItem(e) {
+  e.preventDefault();
+  var li = this.parentNode;
+  li.parentNode.removeChild(li);
 }
